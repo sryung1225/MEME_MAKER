6cd69e580560e865042a39088fb1c48c347f160f
sryung <sryung1225@gmail.com>
2023-01-25 00:11:46 +0900

    [생성] canvas API 탐색 : 그림판 영역 지정, fillRect()

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 import { } from "./lib/index.js";
 
-console.log("hello");
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+canvas.width = 800;
+canvas.height = 800;
+
+ctx.fillRect(50, 50, 300, 300); // x좌표, y좌표, width, height
