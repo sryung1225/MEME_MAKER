7875d19767f0bf687a244b6f88899dcf8f1f816c
sryung <sryung1225@gmail.com>
2023-01-25 17:23:30 +0900

    [생성] 그림판에 그림 그리기 : 마우스 누를 때에만 선그리기 기능 완성

--- app.js
+++ app.js
@@ -4,27 +4,25 @@ const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
-
 ctx.lineWidth = 2;
 
-const colors = [
-  "#ff3838",
-  "#ffb8b8",
-  "#c56cf0",
-  "#ff9f1a",
-  "#32ff7e",
-  "#73fff5",
-  "#18dcff",
-  "#7d5fff",
-];
+let isPainting = false;
 
-function onClick(event){
-  ctx.beginPath();
-  ctx.moveTo(0, 0);
-  const color = colors[Math.floor(Math.random()*colors.length)];
-  ctx.strokeStyle = color;
-  ctx.lineTo(event.offsetX, event.offsetY);
-  ctx.stroke();
+function painting(event){
+  if(isPainting) {
+    ctx.lineTo(event.offsetX, event.offsetY);
+    ctx.stroke();
+  }
+  ctx.moveTo(event.offsetX, event.offsetY);
+}
+function startPainting(){
+  isPainting = true;
+}
+function cancelPainting(){
+  isPainting = false;
 }
 
-canvas.addEventListener("mousemove", onClick);
+canvas.addEventListener("mousemove", painting);
+canvas.addEventListener("mousedown", startPainting);
+canvas.addEventListener("mouseup", cancelPainting);
+canvas.addEventListener("mouseleave", cancelPainting);
