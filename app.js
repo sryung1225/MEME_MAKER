6e179517d4f6a64cb5533415e57cd7f5fa427e90
sryung <sryung1225@gmail.com>
2023-01-25 14:34:22 +0900

    [생성] 그림판에 그림 그리기 : addEventListener 와 결합하기

--- app.js
+++ app.js
@@ -5,45 +5,26 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
-// 집 만들기 ㅋ
-// 지붕
-ctx.moveTo(400, 130);
-ctx.lineTo(100, 270);
-ctx.lineTo(140, 310);
-ctx.lineTo(400, 183);
-ctx.lineTo(660, 310);
-ctx.lineTo(700, 270);
-ctx.fillStyle = "red";
-ctx.fill();
+ctx.lineWidth = 2;
 
-// 집
-ctx.beginPath();
-ctx.moveTo(400, 183);
-ctx.lineTo(203, 280);
-ctx.lineTo(203, 680);
-ctx.lineTo(597, 680);
-ctx.lineTo(597, 280);
-ctx.lineTo(400, 183);
-ctx.fillStyle = "ivory";
-ctx.fill();
-ctx.lineWidth = 5;
-ctx.strokeStyle = "orange";
-ctx.stroke();
+const colors = [
+  "#ff3838",
+  "#ffb8b8",
+  "#c56cf0",
+  "#ff9f1a",
+  "#32ff7e",
+  "#73fff5",
+  "#18dcff",
+  "#7d5fff",
+];
 
-// 창문
-ctx.beginPath();
-ctx.fillStyle = "skyblue";
-ctx.strokeStyle = "brown";
-ctx.arc(400, 340, 80, 1*Math.PI, 2*Math.PI);
-ctx.fill();
-ctx.stroke();
-ctx.fillStyle = "brown";
-ctx.fillRect(310, 340, 180, 10);
+function onClick(event){
+  ctx.beginPath();
+  ctx.moveTo(0, 0);
+  const color = colors[Math.floor(Math.random()*colors.length)];
+  ctx.strokeStyle = color;
+  ctx.lineTo(event.offsetX, event.offsetY);
+  ctx.stroke();
+}
 
-// 문
-ctx.beginPath();
-ctx.fillRect(420, 450, 140, 230);
-ctx.beginPath();
-ctx.fillStyle = "black";
-ctx.arc(450, 565, 10, 0, 2*Math.PI);
-ctx.fill();
+canvas.addEventListener("mousemove", onClick);
