d376d37788d3e57b09017475e742468d1940ba41
sryung <sryung1225@gmail.com>
2023-05-17 00:05:00 +0900

    [✨] 그림판에 그림 그리기 :  배경 채우기 기능 추가 - 그리기 모드 / 배경 채우기 모드 변경 가능 - fillRect()

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import { rgbToHex } from "./lib/index.js";
 
+const modeBtn = document.getElementById("mode-btn");
 const color = document.getElementById("color");
 const colorOptions = Array.from(
   document.getElementsByClassName("color-option")
@@ -11,6 +12,7 @@ canvas.width = 800;
 canvas.height = 800;
 ctx.lineWidth = lineWidth.value;
 let isPainting = false;
+let isFilling = false;
 
 function painting(event) {
   if (isPainting) {
@@ -30,13 +32,29 @@ function cancelPainting() {
 function onLineWidthChange(e) {
   ctx.lineWidth = e.target.value;
 }
+function onModeClick() {
+  if (isFilling) {
+    isFilling = false;
+    modeBtn.innerText = "Fill";
+  } else {
+    isFilling = true;
+    modeBtn.innerText = "Draw";
+  }
+}
+function onCanvasClick() {
+  if (isFilling) {
+    ctx.fillRect(0, 0, 800, 800);
+  }
+}
 function onColorChange(e) {
   ctx.strokeStyle = e.target.value;
+  ctx.fillStyle = e.target.value;
 }
 function onColorClick(e) {
   const colorValue = rgbToHex(getComputedStyle(e.target).backgroundColor);
   console.log(colorValue);
   ctx.strokeStyle = colorValue;
+  ctx.fillStyle = colorValue;
   color.value = colorValue;
 }
 
@@ -44,6 +62,8 @@ canvas.addEventListener("mousemove", painting);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", cancelPainting);
 canvas.addEventListener("mouseleave", cancelPainting);
+canvas.addEventListener("click", onCanvasClick);
 lineWidth.addEventListener("change", onLineWidthChange);
+modeBtn.addEventListener("click", onModeClick);
 color.addEventListener("change", onColorChange);
 colorOptions.forEach((color) => color.addEventListener("click", onColorClick));
