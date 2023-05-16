9336dce66084f4d1e27e89ccc473e31f2146df1c
sryung <sryung1225@gmail.com>
2023-05-16 15:28:59 +0900

    [✨] 그림판에 그림 그리기 : 선 굵기 조절 기능 추가 - beginPath()

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 import {} from "./lib/index.js";
 
+const lineWidth = document.getElementById("line-width");
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
-ctx.lineWidth = 2;
-
+ctx.lineWidth = lineWidth.value;
 let isPainting = false;
 
 function painting(event) {
   if (isPainting) {
     ctx.lineTo(event.offsetX, event.offsetY);
     ctx.stroke();
+    return;
   }
   ctx.moveTo(event.offsetX, event.offsetY);
 }
@@ -20,9 +21,14 @@ function startPainting() {
 }
 function cancelPainting() {
   isPainting = false;
+  ctx.beginPath();
+}
+function onLineWidthChange(e) {
+  ctx.lineWidth = e.target.value;
 }
 
 canvas.addEventListener("mousemove", painting);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", cancelPainting);
 canvas.addEventListener("mouseleave", cancelPainting);
+lineWidth.addEventListener("change", onLineWidthChange);
